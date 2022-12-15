47d6b6d525a2d4eee8b31e6bc98092581cbbf4eb
HugoDemont <hugodemont62@hotmail.fr>
2022-12-15 10:46:23 +0100

    Ajout CSS / SASS / Pages HTML

--- js/main.js
+++ js/main.js
@@ -1,24 +1,14 @@
-let carousel = document.getElementsByClassName(".carousel");
+let carousel = document.querySelector(".carousel");
 
 currdeg = 0;
 
-const next = document.getElementById("next");
-next.addEventListener("click", { d: "n" }, rotate);
-
-const prev = document.getElementById("prev");
-prev.addEventListener("click", { d: "p" }, rotate);
-
-function rotate(e) {
-    if (e.data.d == "n") {
+function rotate(d) {
+    console.log(carousel);
+    if (d == "n") {
         currdeg = currdeg - 60;
     }
-    if (e.data.d == "p") {
+    if (d == "p") {
         currdeg = currdeg + 60;
     }
-    carousel.css({
-        "-webkit-transform": "rotateY(" + currdeg + "deg)",
-        "-moz-transform": "rotateY(" + currdeg + "deg)",
-        "-o-transform": "rotateY(" + currdeg + "deg)",
-        transform: "rotateY(" + currdeg + "deg)",
-    });
+    carousel.style.transform = "rotateY(" + currdeg + "deg)";
 }
